66b92e094a97e9bd313705c916e6911f794ba14f
Aibanez <aibanez@miuandes.cl>
2018-10-02 01:51:52 -0300

    juego atributos

--- Project 7/Project 7/Juego.cs
+++ Project 7/Project 7/Juego.cs
@@ -8,5 +8,16 @@ namespace Project_7
 {
     class Juego
     {
+        private Jugador jugador1;
+        private Jugador jugador2;
+
+        private void CrearJugador1(string nombre)
+        {
+            jugador1 = new Jugador(nombre);
+        }
+        private void CrearJugador2(string nombre)
+        {
+            jugador2 = new Jugador(nombre);
+        }
     }
 }
