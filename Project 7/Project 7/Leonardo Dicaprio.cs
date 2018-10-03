80717fb747fafc2cf983516f96cec19faf989b3f
CristobalLara <clara@miuandes.cl>
2018-10-03 01:11:21 -0300

    Gabiota pa' Dicaprio, wait

---
+++ Project 7/Project 7/Leonardo Dicaprio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_7
+{
+    class Leonardo_Dicaprio : Bitmon
+    {
+        public Leonardo_Dicaprio()
+        {
+            hp = 50;
+            Estamina = 100;
+            ataque = 50;
+            defensa = 50;
+            velocidad = 50;
+            estado = " ";
+            tipo = "Drama";
+            ataques.Add(new Ataque("Frozen", 0, 20, "Drama", 1, "Aumentar Defensa Activo", " ", " "));
+            ataques.Add(new Ataque("Bear Claws", 50, 20, "Suspenso", 1, "Disminuir Hp Oponente", " ", " "));
+            ataques.Add(new Ataque("White Lines", 20, 20, "Drama", 1, "Disminuir Hp Oponente", "Aumentar Ataque Activo", " "));
+            ataques.Add(new Ataque("Spiner", 60, 20, "Comedia", 1, "DisminuirHpOponente", " ", "Confundir"));
+        }
+    }
+}
