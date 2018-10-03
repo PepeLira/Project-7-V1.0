9f7ef25ed42b52cd7daef538e4bdf01aacaeb819
Aibanez <aibanez@miuandes.cl>
2018-10-03 00:47:54 -0300

    tom y sigourney

---
+++ Project 7/Project 7/SigourneyWeaver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_7
+{
+    class SigourneyWeaver:Bitmon
+    {
+        public SigourneyWeaver()
+        {
+            hp = 50;
+            Estamina = 100;
+            ataque = 50;
+            defensa = 50;
+            velocidad = 50;
+            tipo = "Scifi";
+            ataques.Add(new Ataque("Mecha Suit", 60, 20, "Scifi", 1, "DisminuirHpOponente", " ", " "));
+            ataques.Add(new Ataque("Alien Queen", 100, 20, "Scifi", 1, "DisminuirHpActivo", " ", " "));
+            ataques.Add(new Ataque("Cryogenic Sleep", 0, 20, "Scifi", 1, "AumentarHpActivo", " ", "DormirActivo"));
+            ataques.Add(new Ataque("Na'vi Link", 40, 20, "Scifi", 1, "DisminuirHpOponente", " ", " "));
+        }
+    }
+}
